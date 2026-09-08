45c82d130f1408aff0d7f0212d9362bb2e22b24d
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2026-09-08 08:28:32 -0400

    Update BotTester.java

--- src/BotTester.java
+++ src/BotTester.java
@@ -8,6 +8,11 @@ public class BotTester
         String name = input.nextLine();
         
         //Put your code here
+
+        System.out.println("What's the weather like? ");
+
+
+        System.out.println("How many feet are in a mile? ");
         
     }
 }
