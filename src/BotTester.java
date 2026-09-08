a8027ceede4162557d3b42dedd0b063a7484717d
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2026-09-08 08:26:26 -0400

    Update BotTester.java

--- src/BotTester.java
+++ src/BotTester.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class BotTester
 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        
         //Put your code here
         
     }
